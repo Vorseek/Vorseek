0a26671fd9130487489c0cbc972f3a48cd316b20
Michael Belov <mikle0706@icloud.com>
2022-02-07 10:26:15 +0300

    Исправил мобильный скролл

--- src/components/HomePage/Header/index.tsx
+++ src/components/HomePage/Header/index.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import DreiPreview from 'components/HomePage/DreiPreview';
 import styles from './Header.module.css';
-import ArrowIcon from './ArrowIcon';
 
 const Header = () => {
   const ref = React.useRef<HTMLDivElement>(null);
 
-  const ScrollToContent = () => {
-    const clientHeight = ref.current.getClientRects().item(0).height;
-    window.scrollTo({ top: clientHeight, behavior: 'smooth' });
-  };
   return (
     <div ref={ref} className={styles.header}>
       <DreiPreview />
-      <button type="button" onClick={ScrollToContent} className={styles.button}>
-        <ArrowIcon />
-      </button>
     </div>
   );
 };
