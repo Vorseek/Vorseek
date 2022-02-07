120605d8a1449189f25296c3bebef3148fb9654d
Michael Belov <mikle0706@icloud.com>
2022-02-07 10:14:10 +0300

    добавил custom scrollbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import 'assets/styles/globals.css';
+import 'simplebar/dist/simplebar.min.css';
+import SimpleBar from 'simplebar-react';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SimpleBar style={{ maxHeight: '100vh' }}>
+      <Component {...pageProps} />
+    </SimpleBar>
+  );
 }
 
 export default MyApp;
