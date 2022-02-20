bba169762fa16cac9352d0c60d17ea80f65a135f
Mikhail Belov <m.belov@3commas.io>
2022-02-20 12:47:26 +0300

    test netlify.toml

---
+++ src/pages/_error.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Error = ({ statusCode }) => (
+  <div>
+    <h1>error, status: {statusCode}</h1>
+  </div>
+);
+
+Error.getInitialProps = ({ asPath, err, res }) => {
+  const statusCode = res?.statusCode || err?.statusCode || 404;
+
+  if (asPath && asPath !== asPath.toLowerCase()) {
+    res.writeHead(307, { Location: asPath.toLowerCase() });
+    res.end();
+  }
+
+  return { statusCode };
+};
+
+export default Error;
