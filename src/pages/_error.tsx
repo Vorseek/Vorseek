c6c3cc1b84b0c1bdc6201039de03054cfb2355fc
Mikhail Belov <m.belov@3commas.io>
2022-02-21 13:15:14 +0300

    add _errors.tsx

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -6,15 +6,15 @@ const Error = ({ statusCode }) => (
   </div>
 );
 
-// Error.getInitialProps = ({ asPath, err, res }) => {
-//   const statusCode = res?.statusCode || err?.statusCode || 404;
-//
-//   if (asPath && asPath !== asPath.toLowerCase()) {
-//     res.writeHead(307, { Location: asPath.toLowerCase() });
-//     res.end();
-//   }
-//
-//   return { statusCode };
-// };
+Error.getInitialProps = ({ asPath, err, res }) => {
+  const statusCode = res?.statusCode || err?.statusCode || 404;
+
+  if (asPath && asPath !== asPath.toLowerCase()) {
+    res.writeHead(301, { Location: asPath.toLowerCase() });
+    res.end();
+  }
+
+  return { statusCode };
+};
 
 export default Error;
