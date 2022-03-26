46757495171cda1edff1fdbf8b92c9f4660bf99d
Mikhail Belov <m.belov@3commas.io>
2022-03-26 21:24:50 +0300

    fix meta content 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => (
   <div className={styles.container}>
     <Head>
       <title>Vorseek</title>
-      <meta name="description" content="Vorseek 3" />
+      <meta name="description" content="Vorseek 123" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
