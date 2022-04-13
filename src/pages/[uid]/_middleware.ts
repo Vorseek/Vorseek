c2e76af6e5133de061e4e06c09fd2438c687598a
Mikhail Belov <m.belov@3commas.io>
2022-04-13 23:16:47 +0300

    large data test

--- src/pages/[uid]/_middleware.ts
+++ src/pages/[uid]/_middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 function middleware(req: NextRequest) {
   if (req.url.includes('/1')) {
-    return NextResponse.redirect(`${req.url.replace('1', '2')}?redirect=1`);
+    return NextResponse.redirect(`${req.url.replace('/1', '/2')}?redirect=1`);
   }
 
   return NextResponse.next();
