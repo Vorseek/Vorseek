f3b524ca1f5ac5c44f7134ca25355b370fa86798
Mikhail Belov <m.belov@3commas.io>
2022-04-28 20:26:46 +0300

    rewrite in nextconfig

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/blog',
+  // basePath: '/blog',
   reactStrictMode: true,
   i18n: {
     locales: ['en', 'fr', 'nl'],
     defaultLocale: 'en',
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:locale/blog/:path*',
+        destination: '/:locale/:path*',
+      },
+      {
+        source: '/blog/:path*',
+        destination: '/:path*',
+      },
+    ];
   },
 };
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,22 +2,28 @@ import 'assets/styles/globals.css';
 import 'simplebar/dist/simplebar.min.css';
 import SimpleBar from 'simplebar-react';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 
 interface Props extends AppProps {
   stars: number;
   timeInitialProps: string;
 }
 
-const MyApp = ({ Component, pageProps, stars, timeInitialProps }: Props) => (
-  <SimpleBar className="simple-scroll-custom">
-    <Component {...pageProps} stars={stars} timeInitialProps={timeInitialProps} />
-    <h1>
-      {timeInitialProps?.split('\n').map((text) => (
-        <p key={text}>{text}</p>
-      ))}
-    </h1>
-  </SimpleBar>
-);
+const MyApp = ({ Component, pageProps, stars, timeInitialProps }: Props) => {
+  const router = useRouter();
+
+  return (
+    <SimpleBar className="simple-scroll-custom">
+      <h1>Router locale {router.locale}</h1>
+      <Component {...pageProps} stars={stars} timeInitialProps={timeInitialProps} />
+      <h1>
+        {timeInitialProps?.split('\n').map((text) => (
+          <p key={text}>{text}</p>
+        ))}
+      </h1>
+    </SimpleBar>
+  );
+};
 
 MyApp.getInitialProps = async () => {
   // TODO: remove / default function
