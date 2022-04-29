9a9f3404a735008ac39c8aa6332e06eb685f842a
Mikhail Belov <m.belov@3commas.io>
2022-04-30 00:05:02 +0300

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,14 +16,14 @@ const MyApp = ({ Component, pageProps, stars, timeInitialProps }: Props) => (
     {/* scripts zone */}
     <Script
       id="googletagmanager"
-      strategy="worker"
-      src="https://www.googletagmanager.com/gtm.js?id=GTM-WD9827X"
-      onLoad={() => {
-        window.dataLayer = [];
-        window.dataLayer.push({
-          'gtm.start': new Date().getTime(),
-          event: 'gtm.js',
-        });
+      strategy="afterInteractive"
+      // src="https://www.googletagmanager.com/gtm.js?id=GTM-WD9827X"
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-WD9827X');`,
       }}
     />
   </SimpleBar>
