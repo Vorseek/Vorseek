42a9477816f7be8540a7d5e70095c4ae33ef1c8e
Mikhail Belov <m.belov@3commas.io>
2022-04-30 00:59:47 +0300

    large server side for test metrics

--- src/pages/serverside.tsx
+++ src/pages/serverside.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import type { GetServerSideProps } from 'next';
+import axios, { AxiosResponse } from 'axios';
 
 const serverside = ({ min }) => (
   <div>
@@ -11,15 +12,26 @@ const serverside = ({ min }) => (
 export default serverside;
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-  res.setHeader('Cache-Control', 'public, s-maxage=3600, stale-while-revalidate=59');
+  // res.setHeader('Cache-Control', 'public, s-maxage=3600, stale-while-revalidate=59');
 
-  const min = await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(new Date().getMinutes());
-    }, 1000);
+  // const min = await new Promise((resolve) => {
+  // setTimeout(() => {
+  // resolve(new Date().getMinutes());
+  // }, 1000);
+  // });
+  const arr = new Array(1).fill(null);
+
+  const result = [] as Promise<AxiosResponse>[];
+
+  arr.forEach(async () => {
+    const image = axios.get(
+      'https://images.pexels.com/photos/11254131/pexels-photo-11254131.jpeg?cs=srgb&dl=pexels-summer-rune-11254131.jpg&fm=jpg&w=900&h=800'
+    );
+
+    result.push(image);
   });
 
   return {
-    props: { min }, // will be passed to the page component as props
+    props: { res }, // will be passed to the page component as props
   };
 };
