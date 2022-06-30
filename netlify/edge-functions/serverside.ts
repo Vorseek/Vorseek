339b8131e1803c7dcbe0f1d9bf03ab57aa171dde
Mikhail Belov <m.belov@3commas.io>
2022-06-30 16:05:27 +0300

    test geolocation netlify in serverside

--- netlify/edge-functions/serverside.ts
+++ netlify/edge-functions/serverside.ts
@@ -14,16 +14,7 @@ interface Context {
   next: () => Promise<any>;
 }
 
-export default async (request: Request, context: Context) => {
-  const geo = request.headers.get('x-nf-geo');
+export default async (request: Request) =>
+  // const geo = request.headers.get('x-nf-geo');
 
-  const res: Response = await context.next();
-  if (geo) {
-    res.headers.set('x-nf-geo', geo);
-  }
-
-  request.headers.set('X-Your-Custom-Header', 'Your custom header value');
-  res.headers.set('X-Your-Custom-Header', 'Your custom header value');
-
-  return res;
-};
+  new Request(request);
