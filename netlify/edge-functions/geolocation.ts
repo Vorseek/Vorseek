aec4d0726a612059956c6d32133078c151f8a6c3
Mikhail Belov <m.belov@3commas.io>
2022-06-30 15:24:41 +0300

    test geolocation netlify in serverside

--- netlify/edge-functions/geolocation.ts
+++ netlify/edge-functions/geolocation.ts
@@ -11,10 +11,22 @@ interface Context {
     };
   };
   json: (obj: Record<string, any>) => void;
+  next: () => Promise<any>;
 }
 
-export default async (request: Request, context: Context) =>
-  context.json({
+export default async (request: Request, context: Context) => {
+  const geo = request.headers.get('x-nf-geo');
+
+  const res: Response = await context.next();
+  if (geo) {
+    res.headers.set('x-nf-geo', geo);
+  }
+
+  request.headers.set('X-Your-Custom-Header', 'Your custom header value');
+  res.headers.set('X-Your-Custom-Header', 'Your custom header value');
+
+  return context.json({
     geo: context.geo,
     header: request.headers.get('x-nf-geo'),
   });
+};
