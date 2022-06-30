35eff8d47a86d83194418119897a1f9eaea7d970
Mikhail Belov <m.belov@3commas.io>
2022-06-30 17:22:56 +0300

    test geolocation netlify in serverside

--- src/pages/serverside/_middleware.ts
+++ src/pages/serverside/_middleware.ts
@@ -1,13 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(req: NextRequest) {
-  return NextResponse.next();
-
   const { nextUrl: url, geo } = req;
 
   url.searchParams.set('country', geo.country);
 
-  req.headers.set('country-test', geo.country);
+  req.headers.set('country-test', JSON.stringify(geo) || 'unk');
 
   return NextResponse.rewrite(url);
 }
