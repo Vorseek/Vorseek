4d63ff94403d6163e53eb74ef4d402a9c17b08f3
Mikhail Belov <m.belov@3commas.io>
2023-01-05 10:01:33 +0400

    test mozila test security

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,33 +5,49 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect } from 'react';
 
-const Home: NextPage = () => (
-  <div className={styles.container}>
-    <Head>
-      <title>Vorseek</title>
-      <meta name="description" content="Vorseek" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const Home: NextPage = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
 
-    <Header />
+    script.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js');
+    // script.setAttribute('crossorigin', 'anonymous');
+    // script.setAttribute(
+    //   'integrity',
+    //   'sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA=='
+    // );
 
-    <Laptop />
+    document.head.appendChild(script);
+  }, []);
 
-    <Link href="/serverside">to serverside page</Link>
-    <footer className={styles.footer}>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{' '}
-        <span className={styles.logo}>
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </span>
-      </a>
-    </footer>
-  </div>
-);
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Vorseek</title>
+        <meta name="description" content="Vorseek" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <Laptop />
+
+      <Link href="/serverside">to serverside page</Link>
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  );
+};
 
 export default Home;
