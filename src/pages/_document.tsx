7dd6321b6f40b3de8d953ab998ac2442a33b8e4e
Mikhail Belov <m.belov@3commas.io>
2023-01-05 10:23:53 +0400

    test mozila test security

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,8 @@ import { Head, Html, Main, NextScript } from 'next/document';
 const Document = () => (
   <Html lang="ru">
     <Head />
-    <script
-      src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"
-      integrity="sha384-r1y8TJcloKTvouxnYsi4PJAx+nHNr90ibsEn3zznzDzWBN9X3o3kbHLSgcIPtzAp"
-      crossOrigin="anonymous"
-    />
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" />
     <body>
       <Main />
       <NextScript />
