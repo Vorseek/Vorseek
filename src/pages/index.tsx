2b8d49aea7b12eda4ec5d8e7bcf2231fe78588fa
Mikhail Belov <m.belov@3commas.io>
2023-01-05 10:40:49 +0400

    test mozila test security

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,11 +12,11 @@ const Home: NextPage = () => {
     const script = document.createElement('script');
 
     script.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js');
-    script.setAttribute('crossorigin', 'anonymous');
-    script.setAttribute(
-      'integrity',
-      'sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA=='
-    );
+    // script.setAttribute('crossorigin', 'anonymous');
+    // script.setAttribute(
+    //   'integrity',
+    //   'sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA=='
+    // );
 
     document.head.appendChild(script);
   }, []);
